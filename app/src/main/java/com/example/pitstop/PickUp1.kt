1b8a7d75172dba61ed7356f4002c7d152bf9f765
Muhammad Nofa Nurhidayah <muhammadnofa722@gmail.com>
2023-10-17 19:45:12 +0700

    pickup

---
+++ app/src/main/java/com/example/pitstop/PickUp1.kt
@@ -0,0 +1,11 @@
+package com.example.pitstop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PickUp1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pick_up1)
+    }
+}
