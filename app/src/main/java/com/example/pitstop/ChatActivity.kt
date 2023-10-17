1a7b4245879445357f814312e160ba4bf7c32af8
Agym Mahaputra Romy Barlianta <agymjz@gmail.com>
2023-10-17 21:30:27 +0800

    add marketplace

---
+++ app/src/main/java/com/example/pitstop/ChatActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pitstop
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+
+class ChatActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+        val ivexitchat : ImageView = findViewById(R.id.iv_exitchat)
+        ivexitchat.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.iv_exitchat -> {
+                val intent = Intent(this@ChatActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
